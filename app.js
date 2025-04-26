c9d606334f5f72970932edcc30ae06985d2892cf
MahmoodAlnokhatha <mahmoodalnokhtha@gmail.com>
2025-04-26 08:12:23 +0300

    Arrays

--- app.js
+++ app.js
@@ -48,4 +48,26 @@ for (let i = 0; i < nums.length; i++) {
   console.log('  buzz:', buzz);
   console.log('  fizzbuzz:', fizzbuzz);
 
+  const numArrays = [
+    [100, 5, 23],
+    [15, 21, 72, 9],
+    [45, 66],
+    [7, 81, 90]
+  ];
   
+  const numList = numArrays[numArrays.length - 1];
+  
+  console.log('Exercise 13 result:', numList);
+  
+  const num = numArrays[2][1];
+  console.log('Exercise 14 result:', num);
+  
+  let total = 0;
+
+  numArrays.forEach(subArray => {
+    subArray.forEach(num => {
+      total += num;
+    });
+  });
+
+  console.log('Exercise 15 result:\n', total);
